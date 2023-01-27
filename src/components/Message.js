01e00858f5d795fc0e69a09a534a5eef6fbf6f31
Mateo Gomez <mateogomezmolina@gmail.com>
2023-01-26 23:46:13 -0500

    Fix styles when is user message

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,10 +1,13 @@
 export default function Message({ children, isUser = false }) {
-	const color = isUser ? "bg-green-800" : "bg-zinc-600";
-	const side = isUser ? "end" : "start";
+	const classes = isUser
+		? "bg-green-800 rounded-br-none"
+		: "bg-zinc-600 rounded-bl-none";
+
+	const side = isUser ? "justify-end" : "justify-start";
 
 	return (
-		<div className={`flex justify-${side}`}>
-			<p className={`${color} p-2 rounded-lg rounded-bl-none`}>{children}</p>
+		<div className={`flex ${side}`}>
+			<p className={`${classes} p-2 rounded-lg`}>{children}</p>
 		</div>
 	);
 }
